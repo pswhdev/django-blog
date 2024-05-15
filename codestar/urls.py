03455ae63943bf7575eaabc16bfd1932ba252fea
pswhdev <patriciaswhalley@gmail.com>
2024-05-15 12:18:11 +0200

    Add filter to blog vies

--- codestar/urls.py
+++ codestar/urls.py
@@ -16,10 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from blog.views import hello
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('blog/', hello, name='hello'),
     path("", include("blog.urls"), name="blog-urls"),
 ]
